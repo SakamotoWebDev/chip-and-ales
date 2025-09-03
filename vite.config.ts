fa76b86ec9b72405d642c29544538e86227f488f
SakamotoWebDev <gavinsakamoto@comcast.net>
2025-09-02 22:25:27 -0700

    chore: rename repo and update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/chipndales/',
+  base: '/chip-and-ales/',
   plugins: [react()],
   resolve: { 
     alias: { 
